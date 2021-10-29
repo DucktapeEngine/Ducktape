4848246f706b52e2af9399a767410782d0a875e1
TheOkayDev <72171476+Sable-20@users.noreply.github.com>
2021-10-29 12:48:06 -0400

    Delete transform.hpp

--- include/Ducktape/transform.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Transform : public BehaviourScript
-{
-    public:
-        Vector2 position = Vector2(0.0, 0.0);
-        float rotation;
-        Vector2 scale = Vector2(1.0, 1.0);
-};
