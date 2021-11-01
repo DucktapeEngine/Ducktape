9712e89885de246ad09c6112c1c1c19e4f4b4e71
Aryan Baburajan <aryanbaburajan2007@gmail.com>
2021-11-01 12:42:39 +0530

    Fix last merge error

--- include/Ducktape/debug.h
+++ include/Ducktape/debug.h
@@ -4,8 +4,6 @@
 #define FATAL_ERROR 8
 
 #include <stdlib.h>
-#include <stdexcept>
-#include <format>
 
 class Debug
 {
@@ -23,8 +21,8 @@ class Debug
 
         static void LogFatalError(std::string str)
         {
-            std::string errMsg = std::format("\033[0;31m [FATAL ERR]: {} with error code {} \033[0m", str, FATAL_ERROR);
-            throw std::runtime_error(errMsg);
+            std::cout << "\033[0;31m [FATAL ERR] " << str << "\033[0m" << std::endl;
+            exit(FATAL_ERROR);
         }
 
         static void LogWarning(std::string str)
