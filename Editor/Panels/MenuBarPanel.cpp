5acb3e07966b5245c2035c9558e5bf23eadb016c
harrycarriers <harrycarriers@gmail.com>
2022-11-04 03:48:02 +0300

    add GetWindowName and default layout menu item

--- Editor/Panels/MenuBarPanel.cpp
+++ Editor/Panels/MenuBarPanel.cpp
@@ -16,6 +16,14 @@ namespace DT
                 }
                 ImGui::EndMenu();
             }
+            if (ImGui::BeginMenu("Edit"))
+            {
+                if (ImGui::MenuItem("Set Default Layout"))
+                {
+                    RequestDefaultLayout = true;
+                }
+                ImGui::EndMenu();
+            }
             ImGui::EndMainMenuBar();
         }
     }
